a85ae91b08cbdfd047d740b410e642b5ee0f673b
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2024-12-02 21:50:55 +0530

    Add components for animated counter, doughnut chart, and total balance display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,39 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName : 'Nikhil' , lastName : 'Singh' ,email: 'nikhilprataps66@gmail.com'};
   return (
     <div>
-      Home
+        <section className='home'>
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                    type="greeting"
+                    title="Welcome to the app"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions effortlessly." 
+                    />
+                    <TotalBalanceBox
+                       accounts={[]}
+                       totalBanks={1}
+                       totalCurrentBalance={1250.35}
+                    />   
+                </header>
+
+                Recent Transactions
+           
+            </div>
+            <RightSidebar
+               user={loggedIn}
+               transactions={[]}
+               banks={[{ currentBalance: 1253.05},{ currentBalance: 123.05}]}
+
+            
+            />
+        </section>
     </div>
   )
 }
